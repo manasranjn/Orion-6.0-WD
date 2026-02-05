5f6545a2908119d7557945c4b86960c7b8419f89
Manas Ranjan Bera <manasbera484@gmail.com>
2026-02-05 17:57:10 +0530

    js

---
+++ JavaScript/Class 1/script.js
@@ -0,0 +1,24 @@
+// console.log("Hello Everyone");
+
+// var
+// var a = 10
+// console.log(a);
+// var a = 20
+// console.log(a);
+// a = "Hello"
+// console.log(a);
+
+// let
+let b = 10
+// console.log(b);
+// let b
+b = 30
+// console.log(b);
+
+// const
+const c = 1
+// const c
+// c = 10
+// console.log(x);
+
+// y = 10
