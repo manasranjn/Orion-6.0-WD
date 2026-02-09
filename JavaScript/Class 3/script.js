58554ac9c5c6ef7d962c1cb5219db872bbe5f995
Manas Ranjan Bera <manasbera484@gmail.com>
2026-02-09 17:54:40 +0530

    operators

---
+++ JavaScript/Class 3/script.js
@@ -0,0 +1,93 @@
+//! Operators
+//? Arithmetic Operators
+let a = 20
+let b = 3
+// console.log(a + b);
+// console.log(a - b);
+// console.log(a * b);
+// console.log(a / b);
+// console.log(a % b);
+// console.log(a ** b);
+
+//? Asignment Operators
+let x = 10
+x += 5 // x = x+5
+// console.log(x);
+x -= 2
+// console.log(x);
+x *= 2
+// console.log(x);
+// x /= 2
+// console.log(x);
+// x **= 2
+// console.log(x);
+x %= 3
+// console.log(x);
+
+//? Logical Operators
+let p = true
+let q = true
+let r = false
+//* AND
+// console.log(p && q);
+// console.log(p && r);
+// console.log(r && r);
+
+//* OR
+// console.log(p || q);
+// console.log(p || r);
+// console.log(r || r);
+
+//* NOT
+// console.log(!p);
+// console.log(!q);
+// console.log(!r);
+
+//? Comparision Operators
+let c = 10
+let d = 10
+let e = '10'
+let f = 20
+
+// console.log(c == d);
+// console.log(c == e);
+// console.log(c == f);
+// console.log(c != d);
+// console.log(c != f);
+// console.log(c === d);
+// console.log(c === e);
+// console.log(c === f);
+// console.log(c !== d);
+// console.log(c !== e);
+// console.log(c > d);
+// console.log(c > f);
+// console.log(c < d);
+// console.log(c < f);
+// console.log(c <= d);
+// console.log(c >= d);
+
+//? Unary Operators
+let num = 10
+//* Increment
+// Pre-Increment
+// console.log(++num);
+
+// Post-Increment
+// console.log(num++);
+// console.log(num);
+// console.log(num++);
+// console.log(++num)
+
+//* Decrement
+// Pre-Decrement
+// console.log(--num); //9
+
+// Post-Decrement
+// console.log(num--);
+// console.log(--num);
+// console.log(num++);
+// console.log(num++);
+// console.log(--num);
+// console.log(num--);
+// console.log(++num);
+// console.log(--num);
