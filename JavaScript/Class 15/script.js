8e0560af06be277e828f358285b5d7f52d099d7b
Manas Ranjan Bera <manasbera484@gmail.com>
2026-03-05 18:00:32 +0530

    js completed

---
+++ JavaScript/Class 15/script.js
@@ -0,0 +1,60 @@
+// function getData() {
+//     fetch('https://jsonplaceholder.typicode.com/posts')
+//         .then((res) => {
+//             const data = res.json()
+//             console.log(data);
+
+//             // .then((data) => {
+//             //     console.log(data);
+//             // })
+//             // .catch((err) => {
+//             //     console.log("Failed to get the data");
+//             // })
+//         }).catch((err) => {
+//             console.log("Failed to fetch data from API", err);
+//         })
+// }
+// console.log(x);
+
+
+// getData()
+
+async function getData() {
+    const res = await fetch('https://jsonplaceholder.typicode.com/posts') // 1
+    console.log(res); //1
+
+    const data = await res.json() //1
+    console.log(data); //1
+}
+
+// getData()
+
+//! Hoisting
+//? Variable Hoisting
+// console.log(a);
+let a
+// console.log(a);
+// a = 30
+// console.log(a);
+// console.log(x);
+
+var x
+// console.log(x);
+x = 20
+// console.log(x);
+
+//? Function Hoisting
+// greet()
+// const greet = () => {
+//     console.log("hello Everyone");
+
+// }
+// greet()
+
+sayHello()
+
+function sayHello() {
+    console.log("Hello everyone");
+
+}
+sayHello()
