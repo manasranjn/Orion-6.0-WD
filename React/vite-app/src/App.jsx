9f567e1a99b23e850a5a203d12a27ca979813bea
Manas Ranjan Bera <manasbera484@gmail.com>
2026-03-18 18:00:57 +0530

    setup

--- React/vite-app/src/App.jsx
+++ React/vite-app/src/App.jsx
@@ -1,9 +1,18 @@
 import React from "react";
+import Hero from "./Components/Hero";
+import Footer from "./Components/Footer";
+import Navbar from "./Components/Navbar";
+import ClassBase from "./Components/ClassBase";
 
 const App = () => {
   return (
     <div>
+      <Navbar />
       <h1>Hello, this is my first project</h1>
+      <Hero />
+      <Hero></Hero>
+      <Footer />
+      <ClassBase />
     </div>
   );
 };
---
+++ React/vite-app/src/Components/Hero.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Hero = () => {
+  return (
+    <div>
+      <h1>Hello, this is Hero section</h1>
+    </div>
+  );
+};
+
+export default Hero;
